9a5955e960e33d2fdfa6d091044eb07ebc16255e
Pallavi <j.pallavi07@gmail.com>
2020-05-08 11:15:30 -0700

    test

--- code/custom_contentbased.py
+++ code/custom_contentbased.py
@@ -17,7 +17,7 @@ class ContentBasedRecommender:
         vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0.01, max_df=0.80, stop_words=stopwords.words('english'))
         recipe_ids = recipe_df['recipe_id'].tolist()
 
-        self.tfidf_matrix = vectorizer.fit_transform( recipe_df['cook_method'] + "" +recipe_df['ingredients'] + "" + recipe_df['diet_labels'])
+        self.tfidf_matrix = vectorizer.fit_transform( recipe_df['cook_method'] + " " +recipe_df['ingredients'] + " " + recipe_df['diet_labels'])
         #self.tfidf_matrix = vectorizer.fit_transform(recipe_df['clean_ingredients'])
         #self.tfidf_matrix = vectorizer.fit_transform(recipe_df['ingredients'])
 
