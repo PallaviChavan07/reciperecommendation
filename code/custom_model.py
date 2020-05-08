ec4a724042efce29a6c84326f63f9ec8d70e030c
Pallavi <j.pallavi07@gmail.com>
2020-05-08 09:47:22 -0700

    time program and all tasks

--- code/custom_model.py
+++ code/custom_model.py
@@ -23,7 +23,7 @@ recipe_df = pd.read_csv(os.path.realpath('../data/clean/recipes.csv'))
 rating_df = pd.read_csv(os.path.realpath('../data/clean/ratings.csv'))
 user_df = pd.read_csv(os.path.realpath('../data/clean/users.csv'))
 
-user_df = user_df.head(100)
+#user_df = user_df.head(100)
 # valid_users_interaction_df is a subset of rating_df
 valid_users_interaction_df = pd.merge(rating_df, user_df, on='user_id', how='inner')
 merged_df = pd.merge(recipe_df, valid_users_interaction_df, on='recipe_id', how='inner')
